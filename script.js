977a593c4cf3e5ecf7f8f26ed10c60ec1ae18f4d
Arushi Katyal <katy.arushi@gmail.com>
2022-03-11 23:56:52 -0500

    implement more helper functions

--- script.js
+++ script.js
@@ -15290,3 +15290,87 @@ const dictionary = [
   "rural",
   "shave"
 ]
+
+const WORD_LENGTH = 5;
+const guessGrid = document.querySelector("[data-guess-grid]")
+const offsetFromDate = new Date(2022, 0, 1);
+const msOffset = Date.now() - offsetFromDate
+const dayOffset = msOffset / 1000 / 60 / 60 / 24
+const targetWord = targetWords[Math.floor(dayOffset)]
+
+startInteraction()
+
+function startInteraction() {
+  document.addEventListener("click", handleMouseClick)
+  document.addEventListener("keydown", handleKeyPress)
+}
+
+function stopInteraction() {
+  document.removeEventListener("click", handleMouseClick)
+  document.removeEventListener("keydown", handleKeyPress)
+}
+
+function handleMouseClick(e) {
+  if (e.target.matches("[data-key")) {
+    pressKey(e.target.dataset.key)
+    return
+  }
+
+  if (e.target.matches("[data-enter]")) {
+    submitGuess()
+    return
+  }
+
+  if (e.target.matches("[data-delete]")) {
+    deleteKey()
+    return
+  }
+}
+
+function handleKeyPress(e) {
+  if (e.key === "Enter") {
+    submitGuess()
+    return
+  }
+  
+  if (e.key === "Backspace" || e.key === "Delete") {
+    deleteKey()
+  }
+
+  if (e.key.match(/^[a-z]$/)) { // regex for one single letter between a and z
+    pressKey(e.key)
+    return
+  }
+}
+
+function pressKey(key) { // add key to first tile in grid
+  const activeTiles = getActiveTiles()
+  if (activeTiles.length >= WORD_LENGTH) return // make sure that user cannot keep typing after 5 letters
+  const nextTile = guessGrid.querySelector(":not([data-letter])") // allows returns the first tile that matches
+  nextTile.dataset.letter = key.toLowerCase()
+  nextTile.textContent = key
+  nextTile.dataset.state = "active"
+}
+
+function deleteKey() {
+  const activeTiles = getActiveTiles()
+  const lastTile = activeTiles[activeTiles.length - 1] // get the last active tile
+  if (lastTile === null) return
+  lastTile.textContent = ""
+  delete lastTile.dataset.state
+  delete lastTile.dataset.letter
+}
+
+function getActiveTiles() {
+  return guessGrid.querySelectorAll('[data-state="active"]')
+    // return all the tiles that have the state of active
+}
+
+function submitGuess() {
+  const activeTiles = [...getActiveTiles()]
+  if (activeTiles.length !== WORD_LENGTH) { // if the guess isn't long enough, can't submit it!
+    showAlert("Not enough letters!")
+    shakeTiles(activeTiles)
+    return
+  }
+}
